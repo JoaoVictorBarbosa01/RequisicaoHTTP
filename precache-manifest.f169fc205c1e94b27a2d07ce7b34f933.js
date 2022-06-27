fb5c933c49660bcc76a6604391a400599d9093cc
JoaoVictorBarbosa01 <JoaoVictorBarbosa01@users.noreply.github.com>
2022-06-27 02:55:30 +0000

    deploy: 70d131f4e9f44fc155a3de861779fd0be3aaa7cd

---
+++ precache-manifest.f169fc205c1e94b27a2d07ce7b34f933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91a17b679d998d215989",
+    "url": "./static/css/main.aff1997c.chunk.css"
+  },
+  {
+    "revision": "91a17b679d998d215989",
+    "url": "./static/js/main.8e20a97c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "986a461ddad846ecc6f2",
+    "url": "./static/js/2.2ccfc8bd.chunk.js"
+  },
+  {
+    "revision": "55314eea320aa3d0e3a742c655e01e0d",
+    "url": "./index.html"
+  }
+];
