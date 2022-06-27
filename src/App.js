155e80d410a27b3638dec8f9ac7cbd4dba7dccc3
Joao Victor Barbosa <contatojoaovb@gmail.com>
2022-06-27 00:00:14 -0300

    Necessario alteração para o botao

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import'./style.css';
 
-//https://sujeitoprogramador.com/rn-api/?api=posts
+// <a className="botao">Accesar</a> https://sujeitoprogramador.com/rn-api/?api=posts
 
 function App() {
   const [nutri, setNutri] = useState([]);
